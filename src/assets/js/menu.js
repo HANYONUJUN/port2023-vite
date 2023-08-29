c9bd124245d83c3fbba49dcd7f21da47ee08302a
hanyongjun <gksdydwns34@naver.com>
2023-08-29 21:46:42 +0900

    header 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,21 @@
+/* 
+const headerToggle = document.getElementById("headerToggle");
+-> HTML 문서에서 ID가 "headerToggle"인 요소를 변수 headerToggle에 할당
+이는 모바일 토글 버튼 요소를 가리키게 된다.
+
+*/
+
+
 export function menu(){
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
+
+    if(headerToggle){
+        headerToggle.addEventListener("click", ()=>{
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "ture"
+                ?headerToggle.setAttribute("aria-expanded","false")
+                :headerToggle.setAttribute("aria-expanded","true");
+        });
+    }
 }
